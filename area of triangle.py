1abce441e7b5889d40749fbd73227648633b5bd1
Nishi Sharma <84862478+nishisharma24@users.noreply.github.com>
2022-03-08 00:42:52 +0530

    Add files via upload

---
+++ area of triangle.py
@@ -0,0 +1,6 @@
+Length=float(input(" Enter the length of triangle:"))
+Height= float(input("Enter the height of triangle:"))
+Area=(Length*Height)/2
+print(" the area of triangle is",Area)
+input()        
+        
