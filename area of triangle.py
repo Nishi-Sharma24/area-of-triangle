25dd43c0ea6f7f0756339362cc865b124357cda8
Nishi Sharma <84862478+nishisharma24@users.noreply.github.com>
2022-03-08 00:44:15 +0530

    Update area of triangle.py

--- area of triangle.py
+++ area of triangle.py
@@ -1,6 +1,7 @@
+#area of triangle
 Length=float(input(" Enter the length of triangle:"))
-Height= float(input("Enter the height of triangle:"))
+Height=float(input("Enter the height of triangle:"))
 Area=(Length*Height)/2
-print(" the area of triangle is",Area)
+print(" The area of triangle is",Area)
 input()        
         
